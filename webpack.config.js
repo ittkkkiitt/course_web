33706f13212ef1b3c19d2f13a5f972895c6957ae
ittkkkiitt <daygreyki@gmail.com>
2022-12-02 12:29:34 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// const path = require('path');
+// module.exports = {
+//     mode: 'develompment',
+//     entry: {
+//         catalog: './Catalog/catalog.js',
+//         main: './js/root.js',
+//     },
+//     output : {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'dist'),
+//     }
+// }
